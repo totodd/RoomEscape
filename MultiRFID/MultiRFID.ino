0534948fbfa34bf6c6237389871a909110ad4c09
Tao Chen <taochendd@gmail.com>
2018-04-22 21:35:42 +1000

    1

--- MultiRFID/MultiRFID.ino
+++ MultiRFID/MultiRFID.ino
@@ -45,42 +45,52 @@
 
 
 //must have one SS_x_PIN for each reader connected
-#define NR_OF_READERS   3
+#define NR_OF_READERS   2
 
-byte ssPins[] = {SS_1_PIN, SS_2_PIN,SS_3_PIN};
+//indicater of light
+#define indicater1 2
+#define indicater2 3
+#define output 4
+
+//reset button
+#define rst 5
+int lastButtonState = LOW;   // the previous reading from the input pin
+int buttonState;             // the current reading from the input pin
+
+byte ssPins[] = {SS_1_PIN, SS_2_PIN, SS_3_PIN};
 
 MFRC522 mfrc522[NR_OF_READERS];   // Create MFRC522 instance.
 String read_rfid;
 
-//these are hard coded "right" card reads. You will have to change these to match
-//cards you have in inventory - even better this code should be updated
-//so that you can store new valid cards in EEProm.
 
-String ValidCard_1 = "8cc1ad85";
-String ValidCard_2 = "5efc1f2b";
-String ValidCard_3 = "1196b85";
-
-boolean Card_1_ok = false;
-boolean Card_2_ok = false;
-boolean Card_3_ok = false;
 
 //this is the pin the relay is on, change as needed for your code
-#define DoorLockPin 4; 
-
-
-int Read1 = 9;
-int Read2 = 9;
-boolean ValidCardOrder = true;
-String Prevcard = "";
 int NoCardCnt = 0;
-int DoorOpenSec = 2; // 2seconds
+int currentSequence[NR_OF_READERS];
+int cardCount = 0;
+
+// *************customized***************
+
+int targetSequence[] = {1,2};
+int indicaters[] = {indicater1, indicater2};
+
 
 /**
  * Initialize.
  */
 void setup() {
-  pinMode(DoorLockPin,OUTPUT);
-  digitalWrite(DoorLockPin,HIGH);
+  pinMode(indicater1, OUTPUT);
+  pinMode(indicater2, OUTPUT);
+  pinMode(output, OUTPUT);
+
+  pinMode(rst, INPUT);
+
+  
+  digitalWrite(indicater1, LOW);
+  digitalWrite(indicater2, LOW);
+  digitalWrite(output, LOW);
+
+
   Serial.begin(9600); // Initialize serial communications with the PC
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 
@@ -92,54 +102,71 @@ void setup() {
     Serial.print(reader);
     Serial.print(F(": "));
     mfrc522[reader].PCD_DumpVersionToSerial();
-      }
+  }
+
 }
 
 /**
  * Main loop.
  */
 void loop() {
+
+  int buttonReading = digitalRead(rst);
+  debounce(buttonReading);
   
+//  Serial.println(buttonState);
+
+  
+
   if (NoCardCnt > 150){
-    Card_1_ok = false;
-    Card_2_ok = false;
-    Card_3_ok = false;  
-    ValidCardOrder = true;
-    digitalWrite(DoorLockPin,HIGH);
-    NoCardCnt = 0;
-    Prevcard = "";
-    Read1 = 9;
-    Read2 = 9;
+    reset();
   }  
    
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
-    // Look for new cards  
-    
-          
-    //   
-    //Serial.println("Reading readers" + String(ValidCardOrder) + "-----" + String(Card_1_ok) + "--" + String(Card_2_ok) + "--" + String(Card_3_ok));   
+    // Look for new cards
 
     if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
-      Serial.print(F("Reader "));
-      Serial.print(reader);
+      cardCount = cardCount + 1;
+      currentSequence[cardCount-1] = reader+1;
+
+      showCurrentSequence();
+      
+      digitalWrite(indicaters[reader], HIGH);
+
+  
+      Serial.print(F("Reader: "));
+      Serial.println(reader);
       // Show some details of the PICC (that is: the tag/card)
       dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
+      Serial.print(F("RFID:  "));
       Serial.println(read_rfid);
       
-      ValidateCard(reader);
-      
+
       // Halt PICC
       mfrc522[reader].PICC_HaltA();
       // Stop encryption on PCD
       mfrc522[reader].PCD_StopCrypto1();
+
     }
     else{
       NoCardCnt = NoCardCnt + 1;
       delay(50);
-      //Serial.println("No card on sensor cnt"  + String(NoCardCnt));
+    }
+
+
+    if(cardCount == NR_OF_READERS){
+      showCurrentSequence();
+      if(checkSequence(targetSequence, currentSequence)){
+        passAction();
+      }else{
+        failAction();
+      }
     }  
 
-     } 
+  }
+
+  lastButtonState = buttonReading;
+
 }
 
 
@@ -147,76 +174,88 @@ void loop() {
 void dump_byte_array(byte *buffer, byte bufferSize) {
   read_rfid = "";
   for (byte i = 0; i < bufferSize; i++) {
-    read_rfid = read_rfid + String(buffer[i], HEX);
+    read_rfid = read_rfid  + String(buffer[i], HEX);
   }
 }
 
 
 
-void ValidateCard(int reader) {
-   if (reader == Read1) { 
-       Serial.println("Reader 1 already reads"); 
-       goto GoNext;}
-    if (reader == Read2) { 
-       Serial.println("Reader 2 already reads");    
-       goto GoNext;} 
-       
-   int x; 
-   Prevcard = read_rfid;
-    if (Card_1_ok == false && Card_2_ok == false && Card_3_ok == false){
-      if (ValidCard_1 == read_rfid){
-          Read1 = reader;
-          Card_1_ok = true; 
-          NoCardCnt = 0;
-          goto GoNext;           
-      } 
-      else{
-          ValidCardOrder = false;
-          goto GoNext;      
-      } 
-    }
- 
-    if (Card_1_ok == true && Card_2_ok == false && Card_3_ok == false){
-      if (ValidCard_2 == read_rfid){
-          Read2 = reader;
-          Card_2_ok = true; 
-          NoCardCnt = 0;
-          goto GoNext;     
-      } 
-      else{
-          ValidCardOrder = false;
-          goto GoNext;      
-      } 
-    }
- 
-    if (Card_1_ok == true && Card_2_ok == true && Card_3_ok == false){
-      if (ValidCard_3 == read_rfid){
-          Card_3_ok = true;   
-          if (ValidCardOrder) { 
-              open_lock();              
-           }  
-      } 
-      else{
-          ValidCardOrder = false;      
-      } 
-    }
-    
-GoNext:  
-    x = 0;
- 
-}  
-
-
-void open_lock(){
-  //Serial.println("****************************************** Door opened");
-  digitalWrite(DoorLockPin,LOW);
-  delay(DoorOpenSec*1000);
-  digitalWrite(DoorLockPin,HIGH);
-  Card_1_ok = false;
-  Card_2_ok = false;
-  Card_3_ok = false;  
-  ValidCardOrder = true;
+void reset(){
+  digitalWrite(indicater1, LOW);
+  digitalWrite(indicater2, LOW);
+  digitalWrite(output, LOW);
+  cardCount = 0;
   NoCardCnt = 0;
-  Prevcard = "";
-}  
- 
+  for (int i = 0; i < NR_OF_READERS; i++)
+  {
+    currentSequence[i] = 0;
+  }
+}
+
+boolean checkSequence(int target[], int current[]){
+    if (sizeof(target) != sizeof(current))
+    {
+      return false;
+    }else{
+      for (int i = 0; i < sizeof(current); i++) {
+        if (target[i]!=current[i])
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+}
+
+void showCurrentSequence(){
+  for (int i = 0; i < NR_OF_READERS; i++){
+        Serial.print(currentSequence[i]);
+      }
+  Serial.println();
+}
+
+void passAction(){
+  reset();
+  Serial.println("pass");
+  digitalWrite(output, HIGH);
+  delay(2000); // delay 2 sec
+  reset();
+}
+
+void failAction(){
+  reset();
+  Serial.println("fail");
+  for (int i = 0; i < 3; ++i)
+  {
+    digitalWrite(output, HIGH);
+    delay(300);
+    digitalWrite(output, LOW);
+    delay(300);
+  }
+  reset();
+
+}
+
+void debounce(int reading){
+  unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+  unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+    // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+
+    }
+  }
+
+}
