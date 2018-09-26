6f988472d745303dea3521a0a6062f1d9963580a
Tao Chen <tao@quizlingapp.com>
2018-09-27 02:15:30 +1000

    oled module finished

---
+++ module/OLED/OLED.ino
@@ -0,0 +1,90 @@
+/*********************************************************************
+  This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+  This example is for a 128x32 size display using I2C to communicate
+  3 pins are required to interface (2 I2C and one reset)
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada  for Adafruit Industries.
+  BSD license, check license.txt for more information
+  All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+
+
+void setup()   {
+  Serial.begin(9600);
+  initDisplay();
+}
+
+  int num = 1;
+
+void loop() {
+  num++;
+  showTextNumber("Current #: ", num);
+  delay(100);
+
+}
+
+void initDisplay(){
+    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+  display.clearDisplay();
+  display.setRotation(0);
+  display.dim(0);
+}
+
+
+void showTextNumber(String s, int num) {
+  // display.setFont(&FreeMonoBold12pt7b);  // Set a custom font
+    char string[10];
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setTextWrap(true);
+  dtostrf(num, 3, 0, string);
+
+  display.setCursor(0, 0);
+  display.clearDisplay();
+  display.print(s);
+  display.println(string);
+  display.display();
+  // delay(1);
+}
+
+
+void showText(String s) {
+  // display.setFont(&FreeMonoBold12pt7b);  // Set a custom font
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setTextWrap(true);
+
+  display.setCursor(0, 0);
+  display.clearDisplay();
+  display.println(s);
+  display.display();
+  // delay(1);
+}
+
--- module/masterReader/masterReader.ino
+++ module/masterReader/masterReader.ino
@@ -9,43 +9,67 @@
 
 // This example code is in the public domain.
 
-
+#include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define OLED_RESET 4
 
-#define MAX_NR_OF_SLAVES    20
+Adafruit_SSD1306 display(OLED_RESET);
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
 
 #define output 10
-
-
+#define MAX_NR_OF_SLAVES    20
+#define IC1 5
 
 int count = 0;
 int slaveArray[MAX_NR_OF_SLAVES];
-int targetPlaceSequence[MAX_NR_OF_READERS];
-
 void setup() {
   Serial.begin (9600);
+  initDisplay();
+
   scanSlaves();
 
   pinMode(output, OUTPUT);
-
-  for(int i = 0; i < count; i++){
-    targetPlaceSequence[i] = i + 1;
-  }
+  pinMode(IC1, INPUT);
+  delay(1000);
 
 }
 
 int reading[MAX_NR_OF_SLAVES];
-int currentSequence[count];
+
 /////////LOOOOOP/////////////////
 void loop() {
+  for (int i = 0; i < count; i++) {
+    int readed;
+    Wire.requestFrom(slaveArray[i], 1);    // request 6 bytes from slave device #8
 
+    while (Wire.available()) { // slave may send less than requested
+      readed = Wire.read(); // receive a byte as character
+    }
+    reading[i] = readed;
+    Serial.print("slave #");
+    Serial.print(slaveArray[i]);
+    Serial.print(":  ");
+    Serial.print(reading[i]);
+    Serial.print("  ");
+
+  }
+  Serial.println();
 
   if (check_condition(reading)) {
     digitalWrite(output, HIGH);
   } else {
     digitalWrite(output, LOW);
   }
+
+  showTextNumber("Devices: ", count);
+
+  if(digitalRead(IC1)) digitalWrite(output, HIGH);
+  else digitalWrite(output, LOW);
 
   delay(100);
 }
@@ -62,22 +86,25 @@ boolean check_condition(int *arr) {
   return true;
 }
 
-void scanSlaves(){
+void scanSlaves() {
   Serial.println ();
   Serial.println ("I2C scanner. Scanning ...");
   Wire.begin();
+  showText("Scanning..");
+
   for (byte i = 1; i < MAX_NR_OF_SLAVES; i++)
   {
+
     Wire.beginTransmission (i);
     if (Wire.endTransmission () == 0)
     {
       Serial.print ("Found address: ");
       Serial.print (i, DEC);
       Serial.print (" (0x");
-        Serial.print (i, HEX);
-        Serial.println (")");
-        slaveArray[count] = i;
-        count++;
+      Serial.print (i, HEX);
+      Serial.println (")");
+      slaveArray[count] = i;
+      count++;
 
     } // end of good response
     delay (5);  // give devices time to recover
@@ -86,43 +113,46 @@ void scanSlaves(){
   Serial.print ("Found ");
   Serial.print (count, DEC);
   Serial.println (" device(s).");
-
 }
 
-void storeSequence(){
-   for (int i = 0; i < count; i++) {
-    int readed;
-    Wire.requestFrom(slaveArray[i], 1);    // request 6 bytes from slave device #8
-
-    while (Wire.available()) { // slave may send less than requested
-      readed = Wire.read(); // receive a byte as character
-    }
-    reading[i] = readed;
-    Serial.print("slave #");
-    Serial.print(slaveArray[i]);
-    Serial.print(":  ");
-    Serial.print(reading[i]);
-    Serial.print("  ");
-
-  }
-  Serial.println();
-  currentSequence = reading;
-
+void initDisplay() {
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+  display.clearDisplay();
+  display.setRotation(0);
+  display.dim(0);
 }
 
 
+void showTextNumber(String s, int num) {
+  // display.setFont(&FreeMonoBold12pt7b);  // Set a custom font
+  char string[10];
 
-boolean checkSequence(int target[], int current[]){
-    if (sizeof(target) != sizeof(current))
-    {
-      return false;
-    }else{
-      for (int i = 0; i < sizeof(current); i++) {
-        if (target[i]!=current[i])
-        {
-          return false;
-        }
-      }
-      return true;
-    }
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setTextWrap(true);
+  dtostrf(num, 3, 0, string);
+
+  display.setCursor(0, 0);
+  display.clearDisplay();
+  display.print(s);
+  display.println(string);
+  display.display();
+  // delay(1);
 }
+
+
+void showText(String s) {
+  // display.setFont(&FreeMonoBold12pt7b);  // Set a custom font
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setTextWrap(true);
+
+  display.setCursor(0, 0);
+  display.clearDisplay();
+  display.println(s);
+  display.display();
+  // delay(1);
+}
+
