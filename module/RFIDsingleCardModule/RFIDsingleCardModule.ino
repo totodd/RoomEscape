8cdf90ca4efc6301b343e2a592af387151e89ddd
Tao Chen <tao@quizlingapp.com>
2018-10-01 19:11:27 +1000

    gerber

--- module/RFIDsingleCardModule/RFIDsingleCardModule.ino
+++ module/RFIDsingleCardModule/RFIDsingleCardModule.ino
@@ -1,21 +1,22 @@
 /**
-  *
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9                      D9             
- * SPI SS 1    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required **
- * SPI SS 2    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required **
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- *
- */
+
+   Typical pin layout used:
+   -----------------------------------------------------------------------------------------
+               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+   Signal      Pin          Pin           Pin       Pin        Pin              Pin
+   -----------------------------------------------------------------------------------------
+   RST/Reset   RST          9                      D9
+   SPI SS 1    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required *
+   SPI SS 2    SDA(SS)      ** custom, take a unused pin, only HIGH/LOW required *
+   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+
+*/
 
 
+#include <EEPROM.h>
 
 #include <Wire.h>
 #include <SPI.h>
@@ -31,7 +32,7 @@
 
 #define output 6
 #define outputIndicator 6
- byte count = 0;
+byte count = 0;
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
@@ -42,24 +43,29 @@ String cardStored[MAX_NR_OF_CARDS];
 int storedCount = 0;
 int scanCount = 0;
 
+int ledState = LOW;
+boolean isBlinking = false;
+
+
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
 // constants won't change:
-const long interval = 1000;  
+const long interval = 500;
 /**
- * Initialize.
- */
+   Initialize.
+*/
 void setup() {
 
   delay(1000);
   pinMode(output, OUTPUT);
-  pinMode(outputIndicator, OUTPUT); 
+  pinMode(outputIndicator, OUTPUT);
 
   pinMode(modeSwitch, INPUT_PULLUP);
-  digitalWrite(output, LOW);  
-  digitalWrite(outputIndicator, LOW);  
+  digitalWrite(output, LOW);
+  digitalWrite(outputIndicator, LOW);
   autoSetAddr();
 
+  // readSavedInfoROM();
 
   Serial.begin(9600); // Initialize serial communications with the PC
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
@@ -71,91 +77,109 @@ void setup() {
 }
 
 /**
- * Main loop.
- */
+   Main loop.
+*/
 void loop() {
   // if(digitalRead(modeSwitch) == 0){
   //   settingMode = !settingMode;
   // }
-  if(storedCount == 0) settingMode = true;
+  if (storedCount == 0) settingMode = true;
   else settingMode = false;
 
-  if(settingMode){
-    blinkLED(true);
+  Serial.print("Setting mode: ");
+  Serial.print(settingMode);
+  Serial.print("  storedCount:");
+  Serial.print(storedCount);
+  Serial.print("  cardStored 0: ");
+
+  Serial.print(cardStored[0]);
+  Serial.print(" card: ");
+  Serial.println(mfrc522.uid.size);
+
+  if(settingMode && !isBlinking) blinkLED();
+  if(!settingMode) stopBlink();
+
+
+  if (settingMode) {
+    // if (!isBlinking) blinkLED();
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
       Serial.println("card sensed");
+
       cardPresentState = true;
       dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-      if(!inArray(read_rfid, cardStored)){
+
+
+      if (!inArray(read_rfid, cardStored)) {
         cardStored[storedCount] = read_rfid;
+
+        char *tosave = &read_rfid[4];
+        EEPROM_writeAnything(storedCount, tosave);
+        // EEPROM.write(storedCount, read_rfid);
         storedCount++;
-      if(settingMode){
-        blinkLED(1);
-        if(!inArray(read_rfid, cardStored)){
-          cardStored[storedCount] = read_rfid;
-          storedCount++;
-        }
-      }else{
-        blinkLED(0);
-        if(!inArray(read_rfid, cardStored)) waitAction();
-        else{
-          if(read_rfid == cardStored[scanCount]){
+      } else {
+        if (!inArray(read_rfid, cardStored)) waitAction();
+        else {
+          if (read_rfid == cardStored[scanCount]) {
             scanCount++;
-            if(scanCount == storedCount) passAction();
+            if (scanCount == storedCount) passAction();
             else return;
-          }else{
+          } else {
             scanCount = 0;
           }
         }
       }
-    }  else if (!mfrc522.PICC_ReadCardSerial()){
-    cardPresentState = false;
-    waitAction();
-  }
-  }else{
-    blinkLED(false);
+    }  else if (!mfrc522.PICC_ReadCardSerial()) {
+      cardPresentState = false;
+      waitAction();
+    }
+  } else {
+    // if (isBlinking) stopBlink();
     if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
       cardPresentState = true;
       dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-      if(!inArray(read_rfid, cardStored)) waitAction();
-      else{
-        if(read_rfid == cardStored[scanCount]){
+      if (!inArray(read_rfid, cardStored)) waitAction();
+      else {
+        if (read_rfid == cardStored[scanCount]) {
           scanCount++;
-          if(scanCount == storedCount) passAction();
+          if (scanCount == storedCount) passAction();
           else return;
-        }else{
+        } else {
           scanCount = 0;
         }
       }
-    }  else if (!mfrc522.PICC_ReadCardSerial()){
-    cardPresentState = false;
-    waitAction();
-  }
+    }  else if (!mfrc522.PICC_ReadCardSerial()) {
+      cardPresentState = false;
+      waitAction();
+    }
   }
 
   // Serial.println(cardPresentState);
   delay(100);
 
 }
-}
 
-void passAction(){
-  digitalWrite(output,HIGH);
+
+void passAction() {
+  digitalWrite(output, HIGH);
   digitalWrite(outputIndicator, HIGH);
   delay(3000);
   scanCount = 0;
 }
 
-void waitAction(){
-  digitalWrite(output,LOW);
+void waitAction() {
+  digitalWrite(output, LOW);
   digitalWrite(outputIndicator, LOW);
+  isBlinking = false;
 
 }
 
-void blinkLED(boolean state){
-  int ledState = LOW;
-  while(state){
-      unsigned long currentMillis = millis();
+void stopBlink() {
+  digitalWrite(outputIndicator, LOW);
+}
+
+void blinkLED() {
+  isBlinking = true;
+  unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
     // save the last time you blinked the LED
@@ -171,18 +195,18 @@ void blinkLED(boolean state){
     // set the LED with the ledState of the variable:
     digitalWrite(outputIndicator, ledState);
   }
-  }
+
 
 }
 
-boolean inArray(String id, String *array){
+boolean inArray(String id, String *array) {
   for (int n = 0; n < storedCount; n++) {
 
-    if (array[n] != id) {
-      return false;
+    if (array[n] == id) {
+      return true;
     }
   }
-  return true;
+  return false;
 }
 
 void dump_byte_array(byte *buffer, byte bufferSize) {
@@ -197,43 +221,70 @@ void requestEvent() {
   // as expected by master
 }
 
-void autoSetAddr(){
-   Serial.println ();
+void autoSetAddr() {
+  Serial.println ();
   Serial.println ("I2C scanner. Scanning ...");
-   int myAddr = 1;
+  int myAddr = 1;
   int maxAddr = 0;
   Wire.begin();
- for (byte i = 1; i < 30; i++)
- {
-   Wire.beginTransmission (i);
-   if (Wire.endTransmission () == 0)
-     {
-     Serial.print ("Found address: ");
-     Serial.print (i, DEC);
-     Serial.print (" (0x");
-     Serial.print (i, HEX);
-     Serial.println (")");
-     count++;
-     if(i>maxAddr) maxAddr = i;
-     }// end of good response
+  for (byte i = 1; i < 30; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+    {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      if (i > maxAddr) maxAddr = i;
+    }// end of good response
     delay (5);  // give devices time to recover
- } 
- myAddr = maxAddr+1;
+  }
+  myAddr = maxAddr + 1;
   // end of for loop
- Serial.println ("Done.");
- Serial.print ("Found ");
- Serial.print (count, DEC);
- Serial.println (" device(s).");
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
 
 
-   Serial.print ("my Addr is ");
- Serial.println (myAddr);
+  Serial.print ("my Addr is ");
+  Serial.println (myAddr);
 
   Wire.begin(myAddr);                // join i2c bus with address #8
   // Wire.onReceive(receiveEvent); // register event
   Wire.onRequest(requestEvent); // register event
 
 
+}
+
+void readSavedInfoROM(){
+  int cnt = 0;
+  for (int n = 0; n < MAX_NR_OF_CARDS; n++) {
+    cardStored[n] = EEPROM_readAnything(0,read_rfid[4]);
+  }
+}
+
+
+
+template <class T> int EEPROM_writeAnything(int ee, const T& value)
+{
+    const byte* p = (const byte*)(const void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+          EEPROM.write(ee++, *p++);
+    return i;
+}
+
+template <class T> int EEPROM_readAnything(int ee, T& value)
+{
+    byte* p = (byte*)(void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+          *p++ = EEPROM.read(ee++);
+    return i;
 }
 
 
